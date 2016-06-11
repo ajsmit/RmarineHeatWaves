f6205a21c0b83ff2b820a94ee1cc0ad0f5f6f39b
AJ Smit <albertus.smit@gmail.com>
2016-06-11 16:46:09 +0200

    A commit.

--- R/RmarineHeatWaves.R
+++ R/RmarineHeatWaves.R
@@ -1,7 +1,7 @@
 #' Detect Marine Heat Waves and Marine Cold Spells.
 #'
-#'Applies the Hobday et al. (2016) marine heat wave definition to an input time
-#'series of temperature along with a time vector.
+#' Applies the Hobday et al. (2016) marine heat wave definition to an input time
+#' series of temperature along with a time vector.
 #'
 #' @importFrom magrittr %>%
 #' @importFrom plyr .
